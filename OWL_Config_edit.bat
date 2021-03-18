0a673a6775f488b07be4f4182a3700238d8d3c97
Aaron Bloom <abloom@controllogic.com.au>
2021-03-18 14:40:40 +1000

    Added file backup on OWL and local option before edit

--- OWL_Config_edit.bat
+++ OWL_Config_edit.bat
@@ -7,6 +7,7 @@
 ::			Help Section Added
 ::	10/03/2021	Added basic parse for switches
 ::	17/03/2021	Base function implemented
+::	18/03/2021	Added Local backup switch option
 
 ::To Do
 ::
@@ -19,8 +20,7 @@ set returnCode=0
 
 
 if "%*" EQU "" (
-	echo No Input provided
-	GOTO END
+	GOTO HELP
 )
 
 :parse
@@ -28,6 +28,7 @@ set in1=%1
 if "%in1:~0,1%" NEQ "-" GOTO endparse
 if "%in1%" EQU "-d" set debug=Y
 if "%in1%" EQU "-h" GOTO HELP
+if "%in1%" EQU "-b" set backup=Y
 SHIFT
 GOTO parse
 
@@ -42,8 +43,13 @@ GOTO parse
 		echo Debugging by echoing commands
 		@echo on
 	)
+	if "%backup%" EQU "Y" (
+		echo Copying !Cfgfile! to %CD%
+		scp admin@!IPaddr!:/etc/!Cfgfile! %CD%
+
+	)
 	echo SSHing into OWL admin password needed
-	ssh admin@!IPaddr! "Cfgvalue=!Cfgvalue! && sudo sed -i  '/!Cfgline!/ s/.*/'"!Cfgline!=\'$Cfgvalue\'"'/g' /etc/!Cfgfile!"
+	ssh admin@!IPaddr! "Cfgvalue=!Cfgvalue! && sudo sed -i.bak  '/!Cfgline!/ s/.*/'"!Cfgline!=\'$Cfgvalue\'"'/g' /etc/!Cfgfile!"
 
 	GOTO END
 
@@ -51,13 +57,15 @@ GOTO parse
 	Echo OWL CLI Config editor utility
 	Echo This Script was created to get around the forbiden characters issue on the Hirschmann OWL 4G
 	Echo Usage:
-	Echo OWL_Config_edit IPaddress Cfgfile Cfgline Cfgvalue
-	Echo IPaddr = IP address of the OWL 4G eg 192.168.1.1
-	Echo Cfgfile = config file in the OWL 4G to change eg settings.openvpn
-	Echo Cfgline = Line in the configuration file to change eg OPENVPN2_PASSWORD
-	Echo Cfgvalue = value that will be added to the configuration line eg mypassword
-	
-	
+	Echo    OWL_Config_edit [-d -h -b] IPaddress Cfgfile Cfgline Cfgvalue
+	Echo    IPaddr = IP address of the OWL 4G eg 192.168.1.1
+	Echo    Cfgfile = config file in the OWL 4G to change eg settings.openvpn
+	Echo    Cfgline = Line in the configuration file to change eg OPENVPN2_PASSWORD
+	Echo    Cfgvalue = value that will be added to the configuration line eg mypassword
+	Echo Switch options:
+	Echo    -d = Debug script by changing ECHO to ON
+	Echo    -h = Display this help info
+	Echo    -b = Copy setting file to current directory	
 
 	
 	GOTO END
