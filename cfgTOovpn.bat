56a6306143e3feaa3924b81cb9c28400a6bf4abc
Aaron Bloom <abloom@controllogic.com>
2021-06-28 13:29:25 +1000

    Conform coding style to other utilites in project

--- cfgTOovpn.bat
+++ cfgTOovpn.bat
@@ -1,60 +1,99 @@
 :: Control Logic SmartVPN Router config parser Batch file
 
-:: History
-::	30/07/2020 Intial Script Created - Aaron Bloom
-::	27/01/2021 
+:: ###################################History#################################################
+::	30/07/2020 	Intial Script Created - Aaron Bloom
+::	27/01/2021 	??
+::	28/06/2021 	Moved into git
+::			Change format to conform with Aaron's other .bat utilites
 
+::To Do
 ::
-::
+::	Parse basic config
+::	Include all openVPN config options
+
+
 @echo off
-echo This Batch file is meant to convert .cfg files from to .ovpn
+setlocal enableextensions enabledelayedexpansion
+set returnCode=0
 
-set input=%1
 
-IF "%2"=="" (
-	SET output=output.ovpn
-	echo Output set to: %output%
-) ELSE (
-	set output=%2
+if "%*" EQU "" (
+	echo No Input provided
+	GOTO END
 )
 
 
-::if Input is not *.cfg exit
-IF %input~x==.cfg (
-	echo Input is not a .cfg
-	exit /b
-)
+:parse
+set in1=%1
+if "%in1:~0,1%" NEQ "-" GOTO endparse
+if "%in1%" EQU "-d" set debug=Y
+if "%in1%" EQU "-h" GOTO HELP
+SHIFT
+GOTO parse
 
-IF NOT EXIST %input% (
-	echo %input% does not exist
-	exit /b
-)
+:endparse
 
-::if output is not *.ovpn exit
-IF EXIST %output% (
-	::Delete existing file
-	DEL %output%
-	echo Tried to Delete
-)
-
-::Open input
-SETLOCAL ENABLEDELAYEDEXPANSION
-echo Starting Parse
-
-echo client >> %output%
-echo dev tun >> %output%
-
-FOR /F "skip=2 delims=" %%y in ('findstr /R /C:"OPENVPN.*=" %input%') DO (
-	SET temp=%%y
-	SET temp=!temp:OPENVPN=!
-
-	echo !temp:~0,10!
-	echo !temp:*.==!
-	IF /I "!temp:~0,6!"=="_PROTO" (
-		echo  !temp:~7!>>%output%
+:MAIN
+	if "%debug%" EQU "Y" (
+		echo Debugging by echoing commands
+		@echo on
 	)
-	REM To Do setup a switch case based on the inputs DON"T FORGET to find the 
-)
 
-::echo %test:_SearchString=OPEN%
-::	value>>%output%
+	set input=%1
+	IF "%2"=="" (
+		SET output=output.ovpn
+		echo Output set to: %output%
+	) ELSE (
+		set output=%2
+	)
+
+
+	::if Input is not *.cfg exit
+	IF %input~x==.cfg (
+		echo Input is not a .cfg
+		exit /b
+	)
+
+
+	::if output is not *.ovpn exit
+	IF EXIST %output% (
+		::Delete existing file
+		DEL %output%
+		echo Tried to Delete
+	)
+
+
+	::Open input
+	SETLOCAL ENABLEDELAYEDEXPANSION
+	echo Starting Parse
+
+	echo client >> %output%
+	echo dev tun >> %output%
+
+	FOR /F "skip=2 delims=" %%y in ('findstr /R /C:"OPENVPN.*=" %input%') DO (
+		SET temp=%%y
+		SET temp=!temp:OPENVPN=!
+
+		echo !temp:~0,10!
+		echo !temp:*.==!
+		IF /I "!temp:~0,6!"=="_PROTO" (
+			echo  !temp:~7!>>%output%
+		)
+		REM To Do setup a switch case based on the inputs DON"T FORGET to find the 
+	)
+
+	::echo %test:_SearchString=OPEN%
+	::	value>>%output%
+	GOTO END
+
+:HELP
+	Echo Help Section not yet implemented
+	echo This Batch file is meant to convert .cfg files from to .ovpn
+	Echo Usage: cfgTOovpn.bat <switches> input.cfg output.ovpn
+	GOTO END
+
+:END
+	::Pause to allow user to read output and exit
+	PAUSE
+	echo Exiting!
+	exit /B !returnCode!
