18ad93bbdb99feb0b7ffbb97f8d0014669deddc0
Aaron Bloom <abloom@controllogic.com>
2021-08-19 14:08:24 +1000

    Adjusting output to align with OpenVPN config 	Handle the different types of connections and authentication.
    Have not tested extra options
    	Surrounded by double quotes might be an issue

--- cfgTOovpn.bat
+++ cfgTOovpn.bat
@@ -68,37 +68,95 @@ GOTO parse
 	SETLOCAL ENABLEDELAYEDEXPANSION
 	echo Starting Parse
 
-	echo client >> %output%
 	echo dev tun >> %output%
+	::This seems wrong but is in the bash script
+	echo syslog>>%output%
 
 	FOR /F delims^=^ eol^= %%y in ('findstr /R /C:"OPENVPN.*=" %input%') DO (
 
 		SET temp=%%y
 		SET temp=!temp:OPENVPN_=!
-		
 
 		IF /I "!temp:~0,7!"=="ENABLED" echo #Config Enabled=!temp:~8!>>%output%
-		IF /I "!temp:~0,5!"=="PROTO" echo !temp:~6!>>%output%
+		IF /I "!temp:~0,5!"=="PROTO" echo proto !temp:~6!>>%output%
 		IF /I "!temp:~0,11!"=="DESCRIPTION" echo #!temp:~12!>>%output%
-		IF /I "!temp:~0,4!"=="PORT" echo !temp:~5!>>%output%
-		IF /I "!temp:~0,13!"=="REMOTE_IPADDR" echo !temp:~14!>>%output%
-		IF /I "!temp:~0,14!"=="REMOTE_NETWORK" echo !temp:~15!>>%output%
-		IF /I "!temp:~0,14!"=="REMOTE_NETMASK" echo !temp:~15!>>%output%
-		IF /I "!temp:~0,16!"=="REMOTE_IF_IPADDR" echo !temp:~17!>>%output%
-		IF /I "!temp:~0,15!"=="REMOTE_NETWORK6" echo !temp:~16!>>%output%
-		IF /I "!temp:~0,14!"=="REMOTE_PREFIX6" echo !temp:~15!>>%output%
-		IF /I "!temp:~0,17!"=="REMOTE_IF_IPADDR6" echo !temp:~18!>>%output%
-		IF /I "!temp:~0,15!"=="LOCAL_IF_IPADDR" echo !temp:~16!>>%output%
-		IF /I "!temp:~0,16!"=="LOCAL_IF_IPADDR6" echo !temp:~17!>>%output%
-		IF /I "!temp:~0,11!"=="REDIRECT_GW" echo !temp:~12!>>%output%
-		IF /I "!temp:~0,10!"=="PING_INTVL" echo !temp:~11!>>%output%
-		IF /I "!temp:~0,9!"=="PING_TOUT" echo !temp:~10!>>%output%
-		IF /I "!temp:~0,9!"=="RENEG_SEC" echo !temp:~10!>>%output%
-		IF /I "!temp:~0,8!"=="FRAGMENT" echo !temp:~9!>>%output%
+		IF /I "!temp:~0,4!"=="PORT" echo port !temp:~5!>>%output%
+		IF /I "!temp:~0,13!"=="REMOTE_IPADDR" echo remote !temp:~14!>>%output%
+		IF /I "!temp:~0,14!"=="REMOTE_NETWORK" SET REMOTE_NETWORK=!temp:~15!
+		IF /I "!temp:~0,14!"=="REMOTE_NETMASK" echo route !REMOTE_NETWORK! !temp:~15!>>%output%
+		IF /I "!temp:~0,16!"=="REMOTE_IF_IPADDR" SET REMOTE_IF_IPADDR=!temp:~17!
+		IF /I "!temp:~0,15!"=="REMOTE_NETWORK6" SET REMOTE_NETWORK6=!temp:~16!
+		IF /I "!temp:~0,14!"=="REMOTE_PREFIX6" echo route-ipv6 !REMOTE_NETWORK6!/!temp:~15!>>%output%
+		IF /I "!temp:~0,17!"=="REMOTE_IF_IPADDR6" SET REMOTE_IF_IPADDR6=!temp:~18!
+		IF /I "!temp:~0,15!"=="LOCAL_IF_IPADDR" echo ifconfig !temp:~16! REMOTE_IF_IPADDR>>%output%
+		IF /I "!temp:~0,16!"=="LOCAL_IF_IPADDR6" (
+			echo tun-ipv6 >>%output%
+			echo ifconfig-ipv6 !temp:~17! !REMOTE_IF_IPADDR6!>>%output%
+		)
+		IF /I "!temp:~0,11!"=="REDIRECT_GW" (
+			IF /I "!temp:~12!"=="1" echo redirect-gateway>>%output%
+		)
+		IF /I "!temp:~0,10!"=="PING_INTVL" echo ping !temp:~11!>>%output%
+		IF /I "!temp:~0,9!"=="PING_TOUT" (
+			echo ping-restart !temp:~10!>>%output%
+			echo persist-tun>>%output%
+			echo persist-key>>%output%
+		)
+		IF /I "!temp:~0,9!"=="RENEG_SEC" echo reneg-sec !temp:~10!>>%output%
+		IF /I "!temp:~0,8!"=="FRAGMENT" echo fragment !temp:~9!>>%output%
 		IF /I "!temp:~0,4!"=="COMP" echo !temp:~5!>>%output%
-		IF /I "!temp:~0,3!"=="NAT" echo !temp:~4!>>%output%
-		IF /I "!temp:~0,4!"=="AUTH" echo !temp:~5!>>%output%
-		IF /I "!temp:~0,6!"=="SECRET" echo !temp:~17!>>%output%
+		IF /I "!temp:~0,3!"=="NAT" (
+			IF /I "!temp:~4!"=="1" echo ##NAT Needs to be applied - Yet to be implemented>>%output%
+		)
+		IF /I "!temp:~0,4!"=="AUTH" (
+			IF /I "!temp:~5!"=="secret" (
+				echo !temp:~5!>>%output%
+				SET auth=secret
+			)
+			IF /I "!temp:~5!"=="passwd" (
+				echo client>>%output%
+				echo auth-retry nointeract>>%output%
+				echo hand-window 120>>%output%
+				echo key-direction 1 >>%output%
+				SET auth=passwd
+				echo auth-user-pass option please include auth.txt with %output%.ovpn
+				echo auth-user-pass auth.txt>>%output%
+			)
+			IF /I "!temp:~5!"=="tls-mclient" (
+				echo client>>%output%
+				echo auth-retry nointeract>>%output%
+				echo hand-window 120>>%output%
+				echo key-direction 1 >>%output%
+				SET auth=tls
+			)
+			IF /I "!temp:~5!"=="tls-client" (
+				echo tls-client>>%output%
+				echo auth-retry nointeract>>%output%
+				echo hand-window 120>>%output%
+				echo key-direction 1 >>%output%
+				SET auth=tls
+			)
+			IF /I "!temp:~5!"=="tls-server" (
+				echo tls-server>>%output%
+				echo auth-retry nointeract>>%output%
+				echo hand-window 120>>%output%
+				echo key-direction 1 >>%output%
+				SET auth=tls
+			)
+		)
+		IF /I "!temp:~0,6!"=="SECRET" (
+			echo !temp:~7! > %TEMP%\cfgTObat.base64
+			certutil -f -decode %TEMP%\cfgTObat.base64 %TEMP%\cfgTObat.output > NUL
+			IF /I "!auth!"=="secret" echo ^<secret^> >>%output%
+			IF /I "!auth!"=="passwd" echo ^<tls-auth^> >>%output%
+			IF /I "!auth!"=="tls" echo ^<tls-auth^> >>%output%
+			FOR /F delims^=^ eol^= %%D in ('type %TEMP%\cfgTObat.output') DO (
+				echo %%D >>%output%
+			)
+			IF /I "!auth!"=="secret" echo ^</secret^> >>%output%
+			IF /I "!auth!"=="passwd" echo ^</tls-auth^> >>%output%
+			IF /I "!auth!"=="tls" echo ^</tls-auth^> >>%output%
+		)
 		IF /I "!temp:~0,7!"=="CA_CERT" (
 			echo !temp:~8! > %TEMP%\cfgTObat.base64
 			certutil -f -decode %TEMP%\cfgTObat.base64 %TEMP%\cfgTObat.output > NUL
@@ -108,7 +166,15 @@ GOTO parse
 			)
 			echo ^</ca^> >>%output%
 		)
-		IF /I "!temp:~0,9!"=="DH_PARAMS" echo !temp:~10!>>%output%
+		IF /I "!temp:~0,9!"=="DH_PARAMS" (
+			echo !temp:~10! > %TEMP%\cfgTObat.base64
+			certutil -f -decode %TEMP%\cfgTObat.base64 %TEMP%\cfgTObat.output > NUL
+			echo ^<dh^> >>%output%
+			FOR /F delims^=^ eol^= %%D in ('type %TEMP%\cfgTObat.output') DO (
+				echo %%D >>%output%
+			)
+			echo ^</dh^> >>%output%
+		)
 		IF /I "!temp:~0,10!"=="LOCAL_CERT" (
 			echo !temp:~11! > %TEMP%\cfgTObat.base64
 			certutil -f -decode %TEMP%\cfgTObat.base64 %TEMP%\cfgTObat.output > NUL
@@ -127,8 +193,9 @@ GOTO parse
 			)
 			echo ^</key^> >>%output%
 		)
-		IF /I "!temp:~0,8!"=="USERNAME" echo !temp:~9!>>%output%
-		IF /I "!temp:~0,8!"=="PASSWORD" echo !temp:~18!>>%output%
+		IF /I "!temp:~0,8!"=="USERNAME" echo !temp:~9!>>auth.txt
+		IF /I "!temp:~0,8!"=="PASSWORD" echo !temp:~18!>>auth.txt
+		::Outputing in double quotes
 		IF /I "!temp:~0,10!"=="EXTRA_OPTS" echo !temp:~11!>>%output%
 	)
 	echo Removing temporary files
